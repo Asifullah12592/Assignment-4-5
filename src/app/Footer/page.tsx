e202b337bc8cd821f5b4e13b9786c441b3286cfa
Asifullah12592 <Asifullah12592@gmail.com>
2024-10-12 19:09:11 +0500

    Assignment-4-5

---
+++ src/app/Footer/page.tsx
@@ -0,0 +1,14 @@
+export default function Footer(){
+    return(
+
+        <div className="footer">
+            <div><h1>Phon :<br/> <span>03422703531</span></h1></div>
+            <div><h1>Email :<br/> <span><a href="asifkhan125925@gmail.com">asifkhan125925@gmail.com</a></span></h1></div>
+            <div><h1>Facboock :<br/> <span><a href="https://www.facebook.com/profile.php?id=100033490342593&mibextid=ZbWKwL">https://www.facebook.com/profile.php?id=100033490342593&mibextid=ZbWKwL</a></span></h1></div>
+            <div><h1>Whatsapp :<br/> <span><a href="03422703531">03422703531</a></span></h1></div>
+            <div><h1>Github :<br/> <span><a href="https://github.com/Asifullah12592">https://github.com/Asifullah12592</a></span></h1></div>
+          
+          
+        </div>
+    )
+}
